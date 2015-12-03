8be4243da4da1525afa8c7cf40da0e47a0828578
sphantix <sphantix@gmail.com>
2015-12-03 09:05:01 +0800

    Develop | Add string utils

--- crypt/teacrypt.cpp
+++ crypt/teacrypt.cpp
@@ -2,87 +2,90 @@
 
 namespace sol 
 {
-    void CTea::SetKey(uint32_t key[4])
+    namespace crypt
     {
-        _key[0] = key[0];
-        _key[1] = key[1];
-        _key[2] = key[2];
-        _key[3] = key[3];
-    }
-
-    void CTea::tea_encrypt(uint32_t* v, uint32_t* k)
-    {
-        uint32_t v0 = v[0], v1 = v[1], sum = 0, i;
-        uint32_t delta = 0x9e3779b9;
-        uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
-
-        for (i = 0; i < _round; i++)
+        void CTea::SetKey(uint32_t key[4])
         {
-            sum += delta;
-            v0 += ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
-            v1 += ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
+            _key[0] = key[0];
+            _key[1] = key[1];
+            _key[2] = key[2];
+            _key[3] = key[3];
         }
 
-        v[0] = v0;
-        v[1] = v1;
-    }
-
-    void CTea::tea_decrypt(uint32_t* v, uint32_t* k)
-    {
-        uint32_t v0 = v[0], v1 = v[1], sum, i;
-        sum = (_round == 16) ? 0xE3779B90 : 0xC6EF3720;
-
-        uint32_t delta = 0x9e3779b9;
-        uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
-        for (i = 0; i < _round; i++)
+        void CTea::tea_encrypt(uint32_t* v, uint32_t* k)
         {
-            v1 -= ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
-            v0 -= ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
-            sum -= delta;
+            uint32_t v0 = v[0], v1 = v[1], sum = 0, i;
+            uint32_t delta = 0x9e3779b9;
+            uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
+
+            for (i = 0; i < _round; i++)
+            {
+                sum += delta;
+                v0 += ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
+                v1 += ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
+            }
+
+            v[0] = v0;
+            v[1] = v1;
         }
 
-        v[0] = v0;
-        v[1] = v1;
-    }
-
-    bool CTea::Encrypt(const void *input, int input_len, CDataBuffer &out)
-    {
-        if(input == NULL || input_len <= 0)
-            return false;
-
-        int blocks =  (input_len + 7 ) / 8;
-        out.Expand(blocks * 8);
-        out.WriteBytes((const void*)input, input_len);
-        int num = blocks * 8 - input_len;
-        while(num--)
+        void CTea::tea_decrypt(uint32_t* v, uint32_t* k)
         {
-            out.WriteInt8(0);
+            uint32_t v0 = v[0], v1 = v[1], sum, i;
+            sum = (_round == 16) ? 0xE3779B90 : 0xC6EF3720;
+
+            uint32_t delta = 0x9e3779b9;
+            uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
+            for (i = 0; i < _round; i++)
+            {
+                v1 -= ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
+                v0 -= ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
+                sum -= delta;
+            }
+
+            v[0] = v0;
+            v[1] = v1;
         }
 
-        uint32_t *data = (uint32_t *)out.GetData();
-        for(int i = 0; i < blocks; i ++)
+        bool CTea::Encrypt(const void *input, int input_len, CDataBuffer &out)
         {
-            tea_encrypt((uint32_t*)(data + 2 * i), _key);
+            if(input == NULL || input_len <= 0)
+                return false;
+
+            int blocks =  (input_len + 7 ) / 8;
+            out.Expand(blocks * 8);
+            out.WriteBytes((const void*)input, input_len);
+            int num = blocks * 8 - input_len;
+            while(num--)
+            {
+                out.WriteInt8(0);
+            }
+
+            uint32_t *data = (uint32_t *)out.GetData();
+            for(int i = 0; i < blocks; i ++)
+            {
+                tea_encrypt((uint32_t*)(data + 2 * i), _key);
+            }
+
+            return true;
         }
 
-        return true;
-    }
-
-    bool CTea::Decrypt(const void *input, int input_len, CDataBuffer &out)
-    {
-        if(input == NULL || input_len < 8)
-            return false;
-
-        int blocks =  input_len / 8;
-        out.Expand(blocks * 8);
-        out.WriteBytes((const void*)input, blocks * 8);
-
-        uint32_t *data = (uint32_t *)out.GetData();
-        for(int i = 0; i < blocks; i ++)
+        bool CTea::Decrypt(const void *input, int input_len, CDataBuffer &out)
         {
-            tea_decrypt((uint32_t*)(data + 2 * i), _key);
-        }
+            if(input == NULL || input_len < 8)
+                return false;
 
-        return true;
+            int blocks =  input_len / 8;
+            out.Expand(blocks * 8);
+            out.WriteBytes((const void*)input, blocks * 8);
+
+            uint32_t *data = (uint32_t *)out.GetData();
+            for(int i = 0; i < blocks; i ++)
+            {
+                tea_decrypt((uint32_t*)(data + 2 * i), _key);
+            }
+
+            return true;
+        }
     }
 }
---
+++ include/string.h
@@ -0,0 +1,69 @@
+/*
+ * File Name: string.h
+ * Author: Sphantix
+ * Mail: sphantix@gmail.com
+ * Created Time: Thu 03 Dec 2015 08:39:34 AM CST
+ */
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include <string>
+#include <cctype>
+#include <cstddef>
+
+namespace sol
+{
+    namespace string
+    {
+        inline std::string upper(const std::string& s)
+        {
+            std::string upper(s);
+
+            for (std::size_t i = 0; i < s.length(); ++i)
+                upper[i] = std::toupper(upper[i]);
+
+            return upper;
+        }
+
+        inline std::string lower(const std::string& s)
+        {
+            std::string lower(s);
+
+            for (std::size_t i = 0; i < s.length(); ++i)
+                lower[i] = std::tolower(lower[i]);
+
+            return lower;
+        }
+
+        inline std::string trim(const std::string& s)
+        {
+            if (s.length() == 0)
+                return s;
+
+            std::size_t beg = s.find_first_not_of("\a\b\f\n\r\t\v");
+            std::size_t end = s.find_last_not_of("\a\b\f\n\r\t\v");
+
+            if (beg == std::string::npos)
+                return "";
+
+            return std::string(s, beg, end-beg+1);
+        }
+
+        inline std::string& replace_all(std::string& context, const std::string& from, const std::string& to)
+        {
+            std::size_t here = 0;
+            std::size_t found;
+
+            while ((found = context.find(from, here) != std::string::npos))
+            {
+                context.replace(found, from.size(), to);
+                here = found + to.size();
+            }
+
+            return context;
+        }
+    }
+}
+
+#endif /* __STRING_H__ */
